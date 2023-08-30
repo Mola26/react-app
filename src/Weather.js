a5aef203fbb8a3e867a78db30f3bf2b206345fea
Mola26 <nokuthaba2608@gmail.com>
2023-08-30 02:54:55 +0200

    Set up the weather app

---
+++ src/Weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className=" col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Pretoria</h1>
+      <ul>
+        <li>Wednesday 23:00</li>
+        <li>Mostly Sunny</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+            alt="Mostly Sunny"
+          />
+          6℃ | ℉
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 5%</li>
+            <li>Humidity: 60%</li>
+            <li>Wind 12km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
