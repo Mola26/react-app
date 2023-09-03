e266da66bdde38dd4572a3898ec2dd5eb0490691
Mola26 <nokuthaba2608@gmail.com>
2023-09-03 07:53:32 +0200

    Arranged the structure properly

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by {""}
+          <a href="" target="_blank">
+            {" "}
+            Nokuthaba Siphambili
+          </a>
+          {""} and is {""}
+          <a href="" target="_blank">
+            open source on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
